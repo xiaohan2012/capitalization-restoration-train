7a854976dee5ae9df851fbaf801dabdd63ac168c
Han Xiao <han.xiao@cs.helsinki.fi>
2015-05-27 10:54:26 +0300

    added 'capindoc' feature extractor

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,11 +1,13 @@
-import re, string
+import re
+import string
 import enchant
 import nltk
-import string
+
+from util import get_document_content_paf
 
 class Feature(object):
     name = None
-    
+
     @classmethod
     def get_value(cls, t, words, **kwargs):
         raise NotImplementedError
@@ -183,19 +185,66 @@ class AllUppercaseFeature(Feature):
             return False
 
 
+class CapitalizedInDocumentFeature(Feature):
+    """
+    Whether the word appears capitalized in the document.
+
+    One exception is the leading word of the sentence, which is capitalized by convention.
+
+    In this case, we don't consider it as capitalized
+
+    >>> from util import get_document_content_paf, get_document_content
+    >>> doc = get_document_content_paf("/group/home/puls/Shared/capitalization-recovery/10/www.cnbc.com.id.10000030.device.rss.rss/90792FEF7ACEE693A7A87BF5F3D341A1")
+    >>> CapitalizedInDocumentFeature.get_value(0, [u"Shell"], doc=doc)
+    True
+    >>> CapitalizedInDocumentFeature.get_value(0, [u"Van Beurden"], doc=doc)
+    False
+    >>> CapitalizedInDocumentFeature.get_value(0, [u"Getty"], doc=doc)
+    False
+    >>> CapitalizedInDocumentFeature.get_value(0, [u"'Getty"], doc=doc) #some trick
+    False
+    >>> doc = get_document_content_paf("/group/home/puls/Shared/capitalization-recovery/12/www.sacbee.com.business.index/A33DCBDA991E786734BCA02B01B9DB04")
+    >>> CapitalizedInDocumentFeature.get_value(0, [u'Shinjiro'], doc=doc)
+    False
+    >>> CapitalizedInDocumentFeature.get_value(0, [u'Valley'], doc=doc)
+    False
+    >>> CapitalizedInDocumentFeature.get_value(0, [u'Robertson'], doc=doc)
+    True
+    """
+    name = "indoccap"
+
+    @classmethod
+    def _get_label_for_word(cls, word, doc):
+        if not word[0].isalpha():  # stuff like 'robust'
+            return False
+
+        word_capi = unicode(word[0].upper() + word[1:])
+        regexp = re.compile(ur"[0-9a-zA-Z)] %s[ \t\n.,']" %
+                            re.escape(word_capi), re.U)
+
+        return True if regexp.search(doc) else False
+
+    @classmethod
+    def get_value(cls, t, words, **kwargs):
+        doc = kwargs.get("doc")
+        assert doc, "document string should be given"
+        return cls._get_label_for_word(words[t], doc)
+
 DEFAULT_FEATURES = [
     WordFeature, IsLeadingWordFeature,
     LowercaseInDictionaryFeature,
     UppercaseInDictionaryFeature,
     CapitalizedInDictionaryFeature,
-    OriginalInDictionaryFeature, 
+    OriginalInDictionaryFeature,
     AllUppercaseFeature,
-    BeginsWithAlphaFeature, 
+    BeginsWithAlphaFeature,
     ContainsPunctuationFeature,
-    POSFeature, 
+    POSFeature,
+    CapitalizedInDocumentFeature
 ]
 
 # to solve the backward compatability issue
+# Deprecated
 VALUE_LABEL_MAPPING = {
     IsLeadingWordFeature: {True: 'HEAD', False: 'OTHER'},
     LowercaseInDictionaryFeature: {True: 'LOWER_IN_DICT', False: 'OTHER'},
@@ -207,42 +256,51 @@ VALUE_LABEL_MAPPING = {
     ContainsPunctuationFeature: {True: 'HAS_PUNCT', False: 'OTHER'}
 }
 
+
 class FeatureExtractor(object):
     """
     Extract features for sentence
 
     >>> extractor = FeatureExtractor()
-    >>> info = extractor.extract([u"I", u"love", u"you"])
+    >>> info = extractor.extract([u"I", u"love", u"you"], docpath="test_data/i-love-you")
     >>> len(info[0]) == len(DEFAULT_FEATURES)
     True
     >>> info[0]["pos-tag"]
     'PRP'
     >>> info[0]["is-leading-word"]
     True
+    >>> info[1]["indoccap"]
+    True
     >>> extractor.feature_names
-    ['word', 'is-leading-word', 'lower-in-dict', 'upper-in-dict', 'cap-in-dict', 'orig-in-dict', 'all-letter-uppercase', 'begins-with-alphabetic', 'has-punct', 'pos-tag']
+    ['word', 'is-leading-word', 'lower-in-dict', 'upper-in-dict', 'cap-in-dict', 'orig-in-dict', 'all-letter-uppercase', 'begins-with-alphabetic', 'has-punct', 'pos-tag', 'indoccap']
     """
-    def __init__(self, features = DEFAULT_FEATURES):
+    def __init__(self, features=DEFAULT_FEATURES):
         self.features = features
 
-    def extract(self, sent):
+    def extract(self, sent, *args, **kwargs):
         """Expect unicode strings"""
         assert isinstance(sent, list), "sent must be a list"
 
-        kwargs = {}
+        feature_kwargs = {}
+
         if POSFeature in self.features:
-            kwargs["pos"] = [tag for _, tag in nltk.pos_tag(sent)] # pos tags
-                    
+            feature_kwargs["pos"] = [tag for _, tag in nltk.pos_tag(sent)]  # pos tags
+
+        if CapitalizedInDocumentFeature in self.features:
+            assert "docpath" in kwargs
+            feature_kwargs["doc"] = get_document_content_paf(kwargs["docpath"])
+
         words_with_features = []
-        
+
         for i in xrange(len(sent)):
             word = {}
             for feature in self.features:
-                word[feature.name] = feature.get_value(i, sent, **kwargs)                
-            
+                word[feature.name] = feature.get_value(i, sent,
+                                                       **feature_kwargs)
+
             words_with_features.append(word)
 
-        return words_with_features                                
+        return words_with_features
 
     @property
     def feature_names(self):
