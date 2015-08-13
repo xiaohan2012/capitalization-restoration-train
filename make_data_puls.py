4c65b72ee25e1af25d25a73147e5572ea5c471dd
Han Xiao <han.xiao@cs.helsinki.fi>
2015-08-13 14:42:03 +0300

    tested make_data_puls.py

--- make_data_puls.py
+++ make_data_puls.py
@@ -1,17 +1,18 @@
 import sys
 import json
 import traceback
-from data import convert_to_trainable_format
-from cap_transform import make_capitalized_title
 from pathlib import Path
 from toolz.dicttoolz import get_in
-from toolz.functoolz import partial
+from toolz.functoolz import (partial, compose)
 
 from capitalization_restoration.feature_extractor import FeatureExtractor
+from puls_util import get_title_from_puls_core_output
+from data import convert_to_trainable_format
+from cap_transform import make_capitalized_title
 
 
-def printable_train_data(auxil_dir,
-                         paf_dir,
+def printable_train_data(malform_data_dir,
+                         okform_data_dir,
                          ids,
                          extractor, feature_names,
                          start, end=None,
@@ -24,10 +25,10 @@ def printable_train_data(auxil_dir,
 
     Parameters
     ------------
-    auxil_dir: string
-        the directory where all the .auxil files reside
-    paf_dir: string
-        the directory where all the .paf files reside
+    malform_data_dir: string
+        the directory where the malformed data reside
+    okform_data_dir: string
+        the directory where the correctly formed data reside
     ids: list of string
         document ids
     extractor: FeatureExtractor
@@ -47,16 +48,26 @@ def printable_train_data(auxil_dir,
         
     """
     feature_names += ['y']  # add the label feature name
-    auxil_dir = Path(auxil_dir)
+    malform_data_dir = Path(malform_data_dir)
 
+    # take care of this ["tickerSymbol",["NYSE","SKT"]]
+    # /cs/taatto/home/hxiao/capitalization-recovery/corpus/puls-format-capitalized/3987E0BD03749C996A04B881079AD753.auxil
+    clean_tag = (lambda t: t[0] if isinstance(t, list) else t)
     get_tokens = partial(map, partial(get_in, ['token']))
-    get_tags = partial(map, partial(get_in, ['pos']))
+    get_tags = partial(map, compose(clean_tag,
+                                    partial(get_in, ['pos'])))
+    clean_lemma = (lambda t: '--EMPTY--' if len(t) == 0 else t)
+    get_lemmas = partial(map, compose(clean_lemma,
+                                      partial(get_in, ['lemma'])))
+
+    n_collected = 0
 
     for i, id_ in enumerate(ids):
         if i < start:
             continue
             
         if i % 1000 == 0:
+            sys.stderr.write("Collected %d\n" % n_collected)
             sys.stderr.write("Finished %d\n" % i)
 
         if end is not None and i >= end:
@@ -64,13 +75,13 @@ def printable_train_data(auxil_dir,
             break
 
         try:
-            with (auxil_dir / Path(id_)).with_suffix('.auxil')\
-                                     .open(encoding='utf8') as f:
+            with (malform_data_dir / Path(id_)).with_suffix('.auxil')\
+                                               .open(encoding='utf8') as f:
                 # to get the last line
                 for l in f:
                     pass
 
-                docpath = str(paf_dir / Path(id_))
+                docpath = str(okform_data_dir / Path(id_))
 
                 try:
                     data = json.loads(l.strip())
@@ -78,18 +89,77 @@ def printable_train_data(auxil_dir,
                     sys.stderr.write('JSON parse error: {}\n'.format(l))
                     sys.stderr.write(traceback.format_exc())
                     continue
+                    
+                okform_auxil_path = str((okform_data_dir /
+                                         Path(id_)).with_suffix('.auxil'))
+                okform_paf_path = str((okform_data_dir /
+                                       Path(id_)).with_suffix('.paf'))
+                try:
+                    normal_case_titles = get_title_from_puls_core_output(
+                        okform_auxil_path,
+                        okform_paf_path
+                    )
+                except (TypeError, IOError, ValueError):
+                    sys.stderr.write(traceback.format_exc())
+                    continue
+                normal_case_titles = list(normal_case_titles)
+
+                if not isinstance(data['sents'], list):
+                    sys.stderr.write('No headline available\n')
+                    continue
+
+                # we only consider headline that contains only ONE sentence
+                if not (len(normal_case_titles) == 1 and
+                        len(data['sents']) == 1):
+                    sys.stderr.write('Sentence segmentation inconsistent:\n')
+                    sents_str = lambda sents: '\n'.join(
+                        [' '.join(get_tokens(sent['features']))
+                         for sent in sents]
+                    )
+                    sys.stderr.write('Good:\n' + sents_str(normal_case_titles))
+                    sys.stderr.write('Bad:\n' + sents_str(data['sents']))
+                    continue
 
                 try:
-                    for sent in data['sents']:
-                        title_tokens = get_tokens(sent['features'])
-                        tags = get_tags(sent['features'])
-                        words = convert_to_trainable_format(
-                            title_tokens,
-                            title_transform_func,
-                            extractor,
-                            docpath=docpath,
-                            pos=tags
-                        )
+                    good_sent, bad_sent\
+                        = normal_case_titles[0], data['sents'][0]
+
+                    good_title_tokens = get_tokens(good_sent['features'])
+                    bad_title_tokens = get_tokens(bad_sent['features'])
+
+                    # some validity checking
+                    assert len(good_title_tokens) == len(bad_title_tokens)
+                    for good_token, bad_token in zip(good_title_tokens,
+                                                     bad_title_tokens):
+                        assert good_token.lower() == bad_token.lower()
+
+                    tags = get_tags(bad_sent['features'])
+                    lemmas = get_lemmas(bad_sent['features'])
+
+                    # tag validity checking
+                    for tag in tags:
+                        if not (tag is None or
+                                isinstance(tag, basestring)):
+                            raise ValueError(
+                                '{}: tag {} not string'.format(id_, tag)
+                            )
+
+                    # get malformed title tokens
+                    words = convert_to_trainable_format(
+                        good_title_tokens,
+                        title_transform_func,
+                        extractor,
+                        docpath=docpath,
+                        pos=tags,
+                        lemma=lemmas
+                    )
+                except AssertionError:
+                    sys.stderr.write('Sentence content inconsistent:\n')
+                    sys.stderr.write('Good:' + json.dumps(good_title_tokens)
+                                     + '\n')
+                    sys.stderr.write('Bad:' + json.dumps(bad_title_tokens)
+                                     + '\n')
+                    continue
                 except:
                     sys.stderr.write(json.dumps(data) + '\n')
                     sys.stderr.write(traceback.format_exc())
@@ -102,11 +172,11 @@ def printable_train_data(auxil_dir,
                         [unicode(word[feature_name])
                          for feature_name in feature_names])
                     res += word_feature_str + '\n'
-                    
+                n_collected += 1
                 yield res
         except IOError:
             sys.stderr.write('IOError: {}/{}.auxil\n'.format(
-                str(auxil_dir), id_)
+                str(malform_data_dir), id_)
             )
             sys.stderr.write(traceback.format_exc())
 
@@ -117,8 +187,8 @@ if __name__ == '__main__':
     with open('data/doc_ids_2015_08_05.txt', 'r') as f:
         ids = map(lambda s: s.strip(), f.readlines())
 
-    auxil_dir = '/cs/taatto/home/hxiao/capitalization-recovery/corpus/puls-format-capitalized/'
-    paf_dir = '/cs/taatto/home/hxiao/capitalization-recovery/corpus/puls-format/'
+    malform_data_dir = '/cs/taatto/home/hxiao/capitalization-recovery/corpus/puls-format-capitalized/'
+    okform_data_dir = '/cs/taatto/home/hxiao/capitalization-recovery/corpus/puls-format/'
     extractor = FeatureExtractor()
     start = int(sys.argv[1])
     
@@ -127,8 +197,8 @@ if __name__ == '__main__':
     except IndexError:
         end = None
 
-    for l in printable_train_data(auxil_dir,
-                                  paf_dir,
+    for l in printable_train_data(malform_data_dir,
+                                  okform_data_dir,
                                   ids,
                                   extractor=extractor,
                                   feature_names=extractor.feature_names,
---
+++ tests/test_make_data_puls.py
@@ -0,0 +1,63 @@
+import os
+
+from nose.tools import assert_equal
+
+from capitalization_train.make_data_puls import printable_train_data
+from capitalization_train.data import make_capitalized_title
+
+from capitalization_restoration.feature_extractor import (FeatureExtractor,
+                                                          LemmaFeature,
+                                                          POSFeature)
+
+
+CURDIR = os.path.dirname(os.path.realpath(__file__))
+
+extractor = FeatureExtractor([LemmaFeature(), POSFeature()])
+
+
+sent1_json = [{u'lemma': u'nanobiotix',
+               u'pos': u'name_oov',
+               u'token': u'Nanobiotix'},
+              {u'lemma': u'get', u'pos': u'tv', u'token': u'gets'},
+              {u'lemma': u'early', u'pos': u'd', u'token': u'early'},
+              {u'lemma': u'positive', u'pos': u'adj', u'token': u'Positive'},
+              {u'lemma': u'safety', u'pos': u'n', u'token': u'Safety'},
+              {u'lemma': u'result', u'pos': u'n', u'token': u'rEsults'},
+              {u'lemma': u'in', u'pos': u'csn', u'token': u'IN'},
+              {u'lemma': u'head', u'pos': u'n', u'token': u'head'},
+              {u'lemma': u'', u'pos': None, u'token': u'and'},
+              {u'lemma': u'neck', u'pos': u'n', u'token': u'neck'},
+              {u'lemma': u'clinical', u'pos': u'adj', u'token': u'clinical'},
+              {u'lemma': u'trial', u'pos': u'n', u'token': u'trial'}]
+
+
+def test_print_trainable_data():
+    ids = ['001BBB8BFFE6841FA498FCE88C43B63A',
+           '4B4DE4C180DB7697035273DB90BF5101']
+    malform_data_dir = CURDIR + '/data/docs_malformed/'
+    okform_data_dir = CURDIR + '/data/docs_okformed/'
+    res = printable_train_data(malform_data_dir=malform_data_dir,
+                               okform_data_dir=okform_data_dir,
+                               ids=ids,
+                               extractor=extractor,
+                               feature_names=extractor.feature_names,
+                               start=0, end=2,
+                               title_transform_func=make_capitalized_title)
+    sent1 = res.next()
+
+    expected1 = """nanobiotix name_oov IC
+get tv AL
+early d AL
+positive adj IC
+safety n IC
+result n MX
+in csn AU
+head n AL
+--EMPTY-- None AL
+neck n AL
+clinical adj AL
+trial n AL
+"""
+    assert_equal(sent1, expected1)
+
+    assert_equal(len(list(res)), 1)  # 2 sentences in total
