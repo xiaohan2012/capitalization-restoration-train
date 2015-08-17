4e4fd22ecfcd50c6e608a9f1c66433de8128f28e
Han Xiao <han.xiao@cs.helsinki.fi>
2015-08-17 19:41:28 +0300

    tested get_document_body

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,6 @@
 import os
-from nose.tools import assert_equal
-from capitalization_train.util import extract_title
+from nose.tools import assert_equal, assert_true
+from capitalization_train.util import (extract_title, get_document_content_paf)
 
 
 CURDIR = os.path.dirname(os.path.realpath(__file__))
@@ -10,3 +10,14 @@ def test_extract_title():
     actual = extract_title(CURDIR + '/data/docs_okformed/001BBB8BFFE6841FA498FCE88C43B63A')
     expected = u'UPDATE - Nanobiotix gets early Positive Safety rEsults IN head and neck clinical trial'
     assert_equal(actual, expected)
+
+
+def test_get_document_content_paf_empty():
+    actual = get_document_content_paf(CURDIR + '/data/empty_doc')
+    expected = '\n\n'
+    assert_equal(actual, expected)
+
+
+def test_get_document_content_paf():
+    actual = get_document_content_paf(CURDIR + '/data/docs_okformed/001BBB8BFFE6841FA498FCE88C43B63A')
+    assert_true(len(actual.strip()) > 400)
