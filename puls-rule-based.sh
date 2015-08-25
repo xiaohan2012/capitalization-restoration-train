3705b4a2083ce131e43b0f347cc9957396e1be2b
Han Xiao <han.xiao@cs.helsinki.fi>
2015-08-25 16:53:44 +0300

    fix: rule-based classifier

--- puls-rule-based.sh
+++ puls-rule-based.sh
@@ -5,10 +5,10 @@ export SBCL_HOME=/cs/puls/lib/sbcl-1.1.12-x86-64-linux/lib/sbcl
 export PATH=/cs/puls/lib/sbcl-1.1.12-x86-64-linux/bin:$PATH
 export LD_LIBRARY_PATH=/cs/puls/lib/lib64:$LD_LIBRARY_PATH
 
-CORE=/cs/puls/Resources/cores/puls-sbcl-nox-bus-5078.core
-DOC=$2
+CORE=/cs/puls/Resources/cores/64-bit-cores/puls-sbcl-nox-bus.core
+DOC=$1
 
-OP="(process-document-stdout-silently \"$2\" :train-capitalization t :suppress-trace t :validate-capitalization t)"
+OP="(process-document-stdout-silently \"$1\" :suppress-trace t :validate-capitalization t)"
 
 # echo $PATH >> /cs/puls/tmp/sbcl-log
 #which sbcl >> /cs/puls/tmp/sbcl-log
